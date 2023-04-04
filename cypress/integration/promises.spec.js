2bee063a075c4e227bccecda3a01fdf4bf0d5111
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-04-04 00:29:15 -0300

    add-describe

--- cypress/integration/promises.spec.js
+++ cypress/integration/promises.spec.js
@@ -1,1 +1,13 @@
 it('sem testes, ainda',() =>{})
+
+const getsomething = () => 10;
+
+const system = () => {
+    console.log('init');
+    const something = getsomething();
+    console.log(`something is ${something}`);
+    console.log('end')
+
+}
+
+system();
