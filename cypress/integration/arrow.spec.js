f86c49dd0b5533fc93448b33d10a302dfa349876
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-04-03 23:36:43 -0300

    add-ex-of-arrow

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,13 @@
+it('nada agora', function(){})
+
+// function soma(a, b){
+  //  return a + b;
+
+  //const soma = (a, b)=>{
+    //return a +b
+ // }
+    //const soma = (a, b)=> {
+//return a + b
+//}
+
+console.log(soma(1,4))
