022f8f78d5f16bc2e9d74af8508d8826a1e1ac3b
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-17 00:16:45 -0300

    refatoração-test

--- basic-cypress/cypress/e2e/CAC-TAT.cy.js
+++ basic-cypress/cypress/e2e/CAC-TAT.cy.js
@@ -1,14 +1,22 @@
 /// <reference types="Cypress" />
-beforeEach()
+
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    beforeEach(function(){
         cy.visit('./cypress-basico-v2/src/index.html')
+    })
+
+    it('verifica o título da aplicação', function() {
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
 
-    describe('preenche os campos obrigatórios e envia o formulário', function() {
-        
+   // it('preenche os campos obrigatórios e envia o formulário', function(){
+    //    cy.get('input[type="text"]'.type('dalvanir'))
+     //   cy.get('input[type="text"]'.type('vieira'))
+     ///   cy.get('input[type="email"]'.type('dalvanir@dalvani.com'))
+     //   cy.get('textarea[id="open-text-area"]'.type('qualquee coisa'))
+      ////  cy.get('button').click
+       
 
 
-  })
+ // })
 })
