8122b90e9e30d2a7d3b2815d016b917e588fe0e9
dalvanirsv <eeep.dalvani.vieira@gmail.com>
2023-05-16 23:49:55 -0300

    add-new-scenario

--- basic-cypress/cypress/e2e/CAC-TAT.cy.js
+++ basic-cypress/cypress/e2e/CAC-TAT.cy.js
@@ -1,9 +1,14 @@
 /// <reference types="Cypress" />
-
+beforeEach()
 describe('Central de Atendimento ao Cliente TAT', function() {
     it('verifica o título da aplicação', function() {
         cy.visit('./cypress-basico-v2/src/index.html')
         cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
     })
+
+    describe('preenche os campos obrigatórios e envia o formulário', function() {
+        
+
+
   })
-  
+})
